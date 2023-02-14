7adc53258e9002ef29d468d09b73570c41a8a5a6
Simon Mettler <simon@simon-macpro.fritz.box>
2023-02-14 12:07:16 +0100

    basic pug/express setup

---
+++ controllers/overview.js
@@ -0,0 +1,10 @@
+const overview = (req, res) => {
+  res.send('overview');
+}
+
+const overview2 = (req, res) => {
+  res.send('overview2');
+}
+
+//export default overview;
+export {overview, overview2};
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
-import { join, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { join, dirname } from 'node:path';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
 
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+
+
+import { overview, overview2 } from './controllers/overview.js';
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, 'db.json')
@@ -18,7 +23,16 @@ db.data ||= { settings: [], index: []}
 await db.write()
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.use('/overview', overview);
+app.use('/overview2', overview2);
 
 app.listen(port, console.log(`Server listening on port ${port}`))
